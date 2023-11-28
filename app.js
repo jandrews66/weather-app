25ac00360b1e379f9cef37d96ce5f231c4782c9c
Joe Andrews <joeandrews92@gmail.com>
2023-11-27 16:48:06 -0800

    Create functions to display current weather and 3 day forecast

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ async function getWeather(location){
         const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=6c6dcb07e838483dad800431232211&q=${location}&days=3&aqi=no&alerts=no`, {mode: 'cors'})
         const data = await response.json();
         sortData(data);
-
     } catch(error) {
         console.log(error)
     }
@@ -23,29 +22,63 @@ function sortData(data){
     const current = {
         location: data.location.name,
         condition: data.current.condition.text,
-        temp: data.current.temp_c,
-        feels: data.current.feelslike_c,
-        wind: data.current.wind_mph,
-        humidity: data.current.humidity
+        weather: {
+            temp: data.current.temp_c,
+            "feels like": data.current.feelslike_c,
+            wind: data.current.wind_mph,
+            humidity: data.current.humidity
+        }
+    
     };
 
-    const forecast = {
-        location: data.location.name,
-        dayOne: {
+    const forecast = [
+        {
+            date: data.forecast.forecastday[0].date,
             avgTemp: data.forecast.forecastday[0].day.avgtemp_c,
             condition: data.forecast.forecastday[0].day.condition.text
 
         },
-        dayTwo: {
+        {
+            date: data.forecast.forecastday[1].date,
             avgTemp: data.forecast.forecastday[1].day.avgtemp_c,
             condition: data.forecast.forecastday[1].day.condition.text
         },
-        dayThree: {
+        {
+            date: data.forecast.forecastday[1].date,
             avgTemp: data.forecast.forecastday[2].day.avgtemp_c,
             condition: data.forecast.forecastday[2].day.condition.text
         },
 
+    ]
+    renderWeather(current)
+    renderForecast(forecast)
+}
+
+function renderWeather(weatherObj){
+    const condition = document.getElementById("condition")
+    condition.innerHTML = weatherObj.condition
+
+    const location = document.getElementById("location")
+    location.innerHTML = weatherObj.location
+
+    const weatherList = document.getElementById("weather")
+    weatherList.innerHTML = "";
+
+    
+    const obj = weatherObj.weather
+    for (const [key, value] of Object.entries(obj)){
+        const markup = `<li>${key}: ${value}</li>`;
+        weatherList.insertAdjacentHTML('beforeend', markup);
     }
-    console.log(currentWeather);
-    console.log(forecast)
+}
+
+function renderForecast(forecastArray){
+    const forecastList = document.getElementById("forecast")
+    forecastArray.forEach(func)
+    
+    function func(item){
+        const markup = `<li>${item.date}${item.condition}</li>`
+        forecastList.insertAdjacentHTML('beforeend', markup);
+
+    }
 }
